e4f2a97ea426a251f240ad04b5a3f6b575ec3563
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-09 19:45:01 -0400

    more review from prework

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+/*function countdown(i){
+    let i = 10;
+while (i >= 0; i--) {
+  console.log(countdown());
+}
+}*/
+
+function countDown(number) {
+    let x = number
+    while (x >= 0) {
+        console.log(x--)
+    }
+}
